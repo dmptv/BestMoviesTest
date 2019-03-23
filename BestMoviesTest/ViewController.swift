46d3d46b6f36b02fa45e6142949f30a6194a36b1
Kanat Absadykov <thebesttrois@gmail.com>
2019-03-23 10:34:18 +0600

    Initial Commit

---
+++ BestMoviesTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BestMoviesTest
+//
+//  Created by 123 on 23.03.2019.
+//  Copyright © 2019 KaVa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
